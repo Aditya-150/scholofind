3609eae8a675880a3769a6dbf478500ef7b0574f
Aditya <adityaprasadmohanty150@gmail.com>
2022-06-25 19:10:41 +0530

    Added files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Aboutus from "./components/aboutus/Aboutus";
+import Contact from "./components/contact/Contact";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Programs from "./components/programs/Programs";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+        <div className="">
+            <Navbar/>
+        </div>
+        <Header/>
+        <Aboutus/>
+        <Programs/>
+        <Contact/>
+    </div>
+  );
+}
+
+export default App
