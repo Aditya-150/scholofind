30baf7b65a8a9bd79e54099f543b2ae46f2b0e75
Aditya <adityaprasadmohanty150@gmail.com>
2022-06-26 11:17:26 +0530

    Header Processing...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-
+import headerImage from "./../../assets/images/headerImage.png"
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="scholofind__header" id="home">
+      <div className="scholofind__header-content">
+        <h1>
+          Bridging The Gap Between <span>Ambition</span> And{" "}
+          <span>Knowledge</span>
+        </h1>
+        <p>An effort to help each every student all over the world.</p>
+        <div className="scholofind__header-content__button">
+          <button type="button" className="header-content__button-register">
+            Register Now
+          </button>
+          <button type="button" className="header-content__button-know">
+            Know more
+          </button>
+        </div>
+      </div>
+      <div className="scholofind__header-image">
+        <img src={headerImage} alt="headerImage"/>
+      </div>
     </div>
   );
 };
